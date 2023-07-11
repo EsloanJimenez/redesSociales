6f5689da034f0c8999403626eb573814942eb71d
ejpservice <ejptheservice@gmail.com>
2023-07-11 00:09:33 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+
+import './css/App.css';
+
+import background from './images/banner/logo_nuevo_blanco.png'
+import whatsApp from './images/icon/WhatsApp.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='containerMajor'>
+      <figcaption className='figMajor'>
+        <img src={background} alt="rota" />
+      </figcaption>
+
+      <section className='containerSecondary'>
+        <header>
+          <nav>
+            <button><FontAwesomeIcon icon={faEllipsis} /></button>
+          </nav>
+        </header>
+
+        <article className='profile'>
+          <figcaption className='figSecondary'>
+            <img src={background} alt="rota" />
+          </figcaption>
+
+          <h2>@ejpservice</h2>
+        </article>
+
+        <article className='social'>
+          <ul>
+            <li><a href="https://api.whatsapp.com/send?phone=18493308701" target='_blank'><span><img src={whatsApp} /></span><span>WhatsApp</span></a></li>
+            <li><a href="#"><span><img src={whatsApp} /></span><span>WhatsApp</span></a></li>
+            <li><a href="#"><span><img src={whatsApp} /></span><span>WhatsApp</span></a></li>
+            <li><a href="#"><span><img src={whatsApp} /></span><span>WhatsApp</span></a></li>
+            <li><a href="#"><span><img src={whatsApp} /></span><span>WhatsApp</span></a></li>
+          </ul>
+        </article>
+      </section>
+    </section>
   );
 }
 
